4d4074427dc0b858bab1a98a87b8645eddbe885e
Raj Verma <rjnshvrm23@gmail.com>
2020-12-17 22:10:10 +0530

    savingcalculator initial code

---
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -0,0 +1,39 @@
+package com.h2;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+
+public class SavingsCalculator {
+    private float[] credits;
+    private float[] debits;
+
+    public void SavingsCalculator(float credits[], float debits[]){
+        this.credits =credits;
+        this.debits= debits;
+    }
+
+    private float sumOfCredits(){
+        float sum=0.0f;
+        for (float credit : credits){
+            sum += credit;
+        }
+        return sum;
+    }
+
+    private float sumOfDebits(){
+        float sum=0.0f;
+        for (float debit : debits){
+            sum += debit;
+        }
+        return sum;
+    }
+
+    private static int remainingDaysInMonth(LocalDate date){
+
+        YearMonth yearMonth = YearMonth.of(date.getYear(), date.getMonth());
+        int totalDaysInMonth = yearMonth.lengthOfMonth();
+        int remainingdays = totalDaysInMonth - date.getDayOfMonth();
+        return remainingdays;
+    }
+
+}
